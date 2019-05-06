8144511adf34f72f9798048cc77ac3c8a98e1f2d
DivyashreeSV <divyashree.sv96@gmail.com>
2019-05-06 19:49:23 +0530

    06/05/2019

---
+++ src/main/java/com/example/demo/Springdemo1Application.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+@SpringBootApplication
+public class Springdemo1Application {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hey,Welcome to SpringBoot!!!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Springdemo1Application.class, args);
+	}
+}
